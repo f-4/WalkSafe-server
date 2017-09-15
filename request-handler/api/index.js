c89a6a69712011d246aa57f2727d60256770e450
Rick Gallegos <duulketariakan@yahoo.com>
2017-09-15 15:56:08 -0700

    ESlint files

---
+++ request-handler/api/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mapbox from 'mapbox';
+import axios from 'axios';
+
+
+const router = express.Router();
+
+router.use('/map', require('./map'));
+router.use('/user', require('./user'));
+router.use('/auth', require('./auth'));
+
+module.exports = router;
--- request-handler/api/map/map.js
+++ request-handler/api/map/map.js
@@ -13,33 +13,33 @@ const crimeSpot =  (input) => {
           lat: input.lat,
           lon: input.lon,
           key: process.env.SPOTCRIME_API_KEY,
-          radius: 0.01
-        }})
-      .then(result => {
+          radius: 0.01,
+        },
+      },
+    )
+      .then((result) => {
         // Map crimes into annotation objects
-        const crimes = result.data.crimes.map(crime => {
-          return {
-            coordinates: [crime.lat, crime.lon],
-            type: 'point',
-            title: crime.type,
-            subtitle: `${crime.address} ${crime.date}`,
-            annotationImage: {
-              source: { uri: crime.type.toLowerCase() },
-              height: 45,
-              width: 45
-            },
-            id: crime.cdid.toString()
-          }
-        });
+        const crimes = result.data.crimes.map(crime => ({
+          coordinates: [crime.lat, crime.lon],
+          type: 'point',
+          title: crime.type,
+          subtitle: `${crime.address} ${crime.date}`,
+          annotationImage: {
+            source: { uri: crime.type.toLowerCase() },
+            height: 45,
+            width: 45,
+          },
+          id: crime.cdid.toString(),
+        }));
         // Return array of mapped crimes
         console.log('crimes', crimes);
         resolve(crimes);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log('line 52 ', err);
         resolve(err);
       });
-  })
+  });
 };
 
 
@@ -48,8 +48,9 @@ router.get('/geocode/forward', (req, res) => {
   const address = req.query.address;
   console.log('line 46 map.js server search evoked req.session env', req.session)
   mapboxClient.geocodeForward(address, (err) => {
-    if (err) { console.log(err) }
+    if (err) { console.log(err); }
   })
+<<<<<<< 87773fd771a3d9bb206e100c05ca799489dec17f
     .then(result => {
       console.log('Search result', result.entity.features[0]);
       res.send(result.entity.features[0]);
@@ -71,17 +72,24 @@ router.get('/geocode/reverse', (req, res) => {
       res.send(result.entity.features[0]);
     })
     .catch(err => res.status(404).send('Bad Request'));
+=======
+    .then((result) => {
+      console.log('Return search result', result.entity.features[0]);
+      res.send(result.entity.features[0]);
+    });
+>>>>>>> ESlint files
 });
 
 router.get('/crimes', (req, res) => {
   console.log('hey');
   crimeSpot(req.query)
-    .then(crimes => {
-      console.log(crimes)
+    .then((crimes) => {
+      console.log(crimes);
       res.send(crimes);
     })
     .catch(err => res.status(404).send('Bad Request'));
 });
+<<<<<<< 87773fd771a3d9bb206e100c05ca799489dec17f
 
 router.get('/directions', (req, res) => {
   axios.get(`https://api.mapbox.com/directions/v5/mapbox/walking/${req.query.start};${req.query.end}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&geometries=geojson`)
@@ -91,5 +99,7 @@ router.get('/directions', (req, res) => {
     })
     .catch(err => res.status(404).send('Bad Request'));
 });
+=======
+>>>>>>> ESlint files
 
 module.exports = router;
