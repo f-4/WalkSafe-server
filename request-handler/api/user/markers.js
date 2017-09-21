fa7c9cf775d0af2a37410a6c8917ecd1eac2050d
Brian Kim <briankim4311@gmail.com>
2017-09-21 12:25:15 -0700

    Handle GET request to api/user/markers

--- request-handler/api/user/markers.js
+++ request-handler/api/user/markers.js
@@ -6,4 +6,22 @@ const router = express.Router();
 router.use(bodyParser.urlencoded( {extended: true }));
 router.use(bodyParser.json());
 
+router.get('/markers', (req, res) => {
+  const userId = req.query.userId;
+  db.user
+    .findAll({
+      where: {
+        google_id: userId
+      },
+      include:[{
+        model: db.marker
+      }]
+    })
+    .then(result => {
+      console.log(console.log('get markers result', result[0].markers));
+      res.send(result[0].markers);
+    })
+    .catch(console.error);
+});
+
 module.exports = router;
