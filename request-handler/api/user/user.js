af0b8ffbed47c75647ae1619be125da4be1f47cf
Sonrisa <sonrisa722611@gmail.com>
2017-09-21 10:32:03 -0700

    Update endpoint

--- request-handler/api/user/user.js
+++ request-handler/api/user/user.js
@@ -2,7 +2,6 @@ import express from 'express';
 import db from './../../../db/config.js';
 import bodyParser from 'body-parser';
 
-
 const router = express.Router();
 router.use(bodyParser.urlencoded( {extended: true }));
 router.use(bodyParser.json());
